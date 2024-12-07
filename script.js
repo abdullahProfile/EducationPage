3ab247efbf83a6a146c3f7dfd8b57f440c2bed21
Abdullah-CUI <abdullahdev88@gmail.com>
2024-12-07 20:37:06 +0500

    test

---
+++ script.js
@@ -0,0 +1,13 @@
+var menu = document.querySelector(".menu");
+var openMenuButton = document.querySelector(".open-menu");
+var closeMenuButton = document.querySelector(".close-menu");
+
+function openMenu() {
+  menu.style.display = "flex";
+  menu.style.right = "0";
+}
+
+function closeMenu() {
+  menu.style.display = "none";
+  menu.style.right = "-100%";
+}
